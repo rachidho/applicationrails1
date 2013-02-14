b0aa8f16da61fbba6d6ab671aa770f876bb65ba7
rachidho <oumalloukrachid@gmail.com>
2013-02-14 14:27:58 +0100

    premier commit

---
+++ app/controllers/utilisateurs_controller.rb
@@ -0,0 +1,10 @@
+class UtilisateursController < ApplicationController
+  
+  def index
+    @utilisateurs = Utilisateur.all
+  end
+  
+  def show
+    @utilisateur = Utilisateur.find params[:id]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applicatiorails1::Application.config.secret_token = '5d483aafc6c5e185fd788323120197efadb0cee3234546ea13832516a985f73b6f3e4669f07ba1de8dffde00e307aaf4d12cc7789c7bdfd2bad367a2c3ecce8f'
